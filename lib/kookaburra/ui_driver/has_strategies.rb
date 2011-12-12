9f702baee42fe94be1d31f26dd5674c8decc953a
Sam Livingston-Gray <geeksam@gmail.com>
2011-12-11 16:08:01 -0800

    minor hierarchy refactor

--- lib/kookaburra/ui_driver/has_strategies.rb
+++ lib/kookaburra/ui_driver/has_strategies.rb
@@ -1,6 +1,6 @@
 module Kookaburra
   class UIDriver
-    module HasStrategies
+    class UIComponent
       class Strategy
         __cattr_accessor_for_kookaburra(:tag)
         attr_reader :ui_component
@@ -9,40 +9,42 @@ module Kookaburra
         end
       end
 
-      module ClassMethods
-        def strategy(tag, &proc)
-          Class.new(::Kookaburra::UIDriver::HasStrategies::Strategy).tap { |klass|
-            klass.tag = tag
-            klass.module_eval &proc
-            self.strategy_classes << klass
-          }
+      module HasStrategies
+        module ClassMethods
+          def strategy(tag, &proc)
+            Class.new(::Kookaburra::UIDriver::UIComponent::Strategy).tap { |klass|
+              klass.tag = tag
+              klass.module_eval &proc
+              self.strategy_classes << klass
+            }
+          end
+
+          def use_strategy_for(*method_names)
+            def_delegators :current_strategy, *method_names
+          end
         end
 
-        def use_strategy_for(*method_names)
-          def_delegators :current_strategy, *method_names
+        module InstanceMethods
+          def strategies
+            @strategies ||= strategy_classes.map { |klass| klass.new(self) }
+          end
+
+          def current_strategy
+            strategies.detect(&:applies?) or raise 'No applicable strategy!'
+          end
+
+          def strategy_tag
+            current_strategy.tag
+          end
         end
-      end
 
-      module InstanceMethods
-        def strategies
-          @strategies ||= strategy_classes.map { |klass| klass.new(self) }
+        def self.included(receiver)
+          receiver.__cattr_accessor_for_kookaburra :strategy_classes, []
+
+          receiver.extend Forwardable
+          receiver.extend         ClassMethods
+          receiver.send :include, InstanceMethods
         end
-
-        def current_strategy
-          strategies.detect(&:applies?) or raise 'No applicable strategy!'
-        end
-
-        def strategy_tag
-          current_strategy.tag
-        end
-      end
-
-      def self.included(receiver)
-        receiver.__cattr_accessor_for_kookaburra :strategy_classes, []
-
-        receiver.extend Forwardable
-        receiver.extend         ClassMethods
-        receiver.send :include, InstanceMethods
       end
     end
   end
