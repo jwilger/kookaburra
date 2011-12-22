2d66c7e781eb0527774a0b84d70432c588e0c610
Sam Livingston-Gray <geeksam@gmail.com>
2011-12-21 16:21:06 -0800

    Stop doing Foo::Bar::Baz, rename setup function.
    The Foo::Bar::Baz thing gets borked when you #require files in the order provided by Dir.glob.

--- lib/kookaburra/world_setup.rb
+++ lib/kookaburra/world_setup.rb
@@ -1,10 +1,12 @@
 # Can't use a custom World class, because cucumber-rails is already doing that
-module Kookaburra::WorldSetup
-  def ui;    @drivers[:ui_driver   ]; end
-  def api;   @drivers[:api_driver  ]; end
-  def given; @drivers[:given_driver]; end
+module Kookaburra
+  module WorldSetup
+    def ui;    @drivers[:ui_driver   ]; end
+    def api;   @drivers[:api_driver  ]; end
+    def given; @drivers[:given_driver]; end
 
-  def __kookaburra_setup
-    @drivers = Kookaburra.drivers
+    def kookaburra_world_setup
+      @drivers = Kookaburra.drivers
+    end
   end
 end
