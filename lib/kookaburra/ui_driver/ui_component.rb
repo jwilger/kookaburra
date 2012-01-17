00c7f72f7336992a1844b3adb4dc5a93d0445a91
John Wilger <johnwilger@gmail.com>
2012-01-16 21:13:46 -0800

    Clean up unnecessary method

--- lib/kookaburra/ui_driver/ui_component.rb
+++ lib/kookaburra/ui_driver/ui_component.rb
@@ -40,13 +40,8 @@ module Kookaburra
 
       def visible?
         no_500_error!
-        _visible?
-      end
-
-      def _visible?
         component_visible?
       end
-      private :_visible?
 
       def show(opts = {})
         return if visible?
