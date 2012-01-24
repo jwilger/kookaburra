63cccb766ac6d04d4895c83a88423b9e26581f78
John Wilger <johnwilger@gmail.com>
2012-01-23 20:06:38 -0800

    added APIDriver#get_as_json

--- lib/kookaburra/api_driver.rb
+++ lib/kookaburra/api_driver.rb
@@ -20,21 +20,30 @@ module Kookaburra
     ##### JSON Tools #####
 
     def post_as_json(short_description, path, data = {}, options = {})
-      header 'Content-Type', 'application/json'
-      header 'Accept', 'application/json'
+      set_json_request_headers
       post path, data.to_json
       raise_unless_status options[:expected_status] || 201, short_description
     end
 
     def put_as_json(short_description, path, data = {}, options = {})
-      header 'Content-Type', 'application/json'
-      header 'Accept', 'application/json'
+      set_json_request_headers
       put path, data.to_json
       raise_unless_status options[:expected_status] || 201, short_description
     end
 
+    def get_as_json(short_description, path, data = {}, options = {})
+      set_json_request_headers
+      get path, data
+      raise_unless_status options[:expected_status] || 200, short_description
+    end
+
     def hash_from_response_json
       HashWithIndifferentAccess.new( JSON.parse(last_response.body) )
+    end
+
+    def set_json_request_headers
+      header 'Content-Type', 'application/json'
+      header 'Accept', 'application/json'
     end
   end
 end
