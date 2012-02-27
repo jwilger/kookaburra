90e9ccc85de8ed158988e8bc181fe413a00ee425
John Wilger <johnwilger@gmail.com>
2012-02-27 15:12:40 -0800

    Simplify test

--- lib/kookaburra.rb
+++ lib/kookaburra.rb
@@ -2,7 +2,4 @@ class Kookaburra
   require 'kookaburra/api_driver'
   require 'kookaburra/given_driver'
   require 'kookaburra/ui_driver'
-
-  def initialize(params = {})
-  end
 end
--- spec/kookaburra_integration_spec.rb
+++ spec/kookaburra_integration_spec.rb
@@ -7,30 +7,28 @@ describe 'Kookaburra Integration' do
     describe "with an HTML interface" do
       describe "with a JSON API" do
         it "runs the tests against the app" do
-          my_api_driver_class = Class.new(Kookaburra::APIDriver)
-          my_given_driver_class = Class.new(Kookaburra::GivenDriver)
-          my_ui_driver_class = Class.new(Kookaburra::UIDriver)
-          my_app = Object.new
-          my_session = Capybara::Session.new(:javascript, my_app)
-          my_api_driver = my_api_driver_class.new(:app => my_app)
-          my_given_driver = my_given_driver_class.new(:api_driver => my_api_driver)
-          my_ui_driver = my_ui_driver_class.new(:session => my_session)
-          k = Kookaburra.new(:given_driver => my_given_driver, :ui_driver => my_ui_driver)
+          # Set up GivenDriver for this test
+          my_given_driver_class = Class.new(Kookaburra::GivenDriver) do
+            def a_user(name)
+            end
+          end
+          given = my_given_driver_class.new
 
+
+          given.a_user(:bob)
           pending 'WIP' do
-            k.given.a_user(:bob)
-            k.given.a_widget(:widget_a)
-            k.given.a_widget(:widget_b, :name => 'Foo')
+            given.a_widget(:widget_a)
+            given.a_widget(:widget_b, :name => 'Foo')
 
-            k.ui.sign_in(:bob)
-            k.ui.navigate_to :widget_list
-            k.ui.widget_list.widgets.should == [k.widgets[:widget_a], k.widgets[:widget_b]]
+            ui.sign_in(:bob)
+            ui.navigate_to :widget_list
+            ui.widget_list.widgets.should == [k.widgets[:widget_a], k.widgets[:widget_b]]
 
-            k.ui.create_new_widget(:widget_c, :name => 'Bar')
-            k.ui.widget_list.widgets.should == [k.widgets[:widget_a], k.widgets[:widget_b], k.widgets[:widget_c]]
+            ui.create_new_widget(:widget_c, :name => 'Bar')
+            ui.widget_list.widgets.should == [k.widgets[:widget_a], k.widgets[:widget_b], k.widgets[:widget_c]]
 
-            k.ui.delete_widget(:widget_b)
-            k.ui.widget_list.widgets.should == [k.widgets[:widget_a], k.widgets[:widget_c]]
+            ui.delete_widget(:widget_b)
+            ui.widget_list.widgets.should == [k.widgets[:widget_a], k.widgets[:widget_c]]
           end
         end
       end
