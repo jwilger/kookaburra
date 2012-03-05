1eef6c61e217f26df71d593b9b028a6f6d809913
John Wilger <johnwilger@gmail.com>
2012-03-05 15:18:10 -0800

    better variable name

--- lib/kookaburra/dependency_accessor.rb
+++ lib/kookaburra/dependency_accessor.rb
@@ -3,12 +3,12 @@ require 'active_support/core_ext/string'
 class Kookaburra
   module DependencyAccessor
     def dependency_accessor(*names)
-      names.each do |n|
-        define_method(n) do
-          instance_variable_get("@#{n}") or raise "No %s object was set on %s initialization." \
-            % [n, [self.class.name, 'an Anonymous Class!!!'].reject(&:blank?).first]
+      names.each do |name|
+        define_method(name) do
+          instance_variable_get("@#{name}") or raise "No %s object was set on %s initialization." \
+            % [name, [self.class.name, 'an Anonymous Class!!!'].reject(&:blank?).first]
         end
-        private n
+        private name
       end
     end
   end
