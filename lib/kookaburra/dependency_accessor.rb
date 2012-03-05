9154b51e19a09dac414a6efe0393740479c169ee
John Wilger <johnwilger@gmail.com>
2012-03-05 15:49:49 -0800

    extract method

--- lib/kookaburra/dependency_accessor.rb
+++ lib/kookaburra/dependency_accessor.rb
@@ -3,13 +3,17 @@ require 'active_support/core_ext/string'
 class Kookaburra
   module DependencyAccessor
     def dependency_accessor(*names)
-      names.each do |name|
-        define_method(name) do
-          instance_variable_get("@#{name}") or raise "No %s object was set on %s initialization." \
-            % [name, [self.class.name, 'an Anonymous Class!!!'].reject(&:blank?).first]
-        end
-        private name
+      names.each { |name| define_dependency_accessor(name) }
+    end
+
+    private
+
+    def define_dependency_accessor(name)
+      define_method(name) do
+        instance_variable_get("@#{name}") or raise "No %s object was set on %s initialization." \
+          % [name, [self.class.name, 'an Anonymous Class!!!'].reject(&:blank?).first]
       end
+      private name
     end
   end
 end
