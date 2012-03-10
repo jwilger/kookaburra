ca8c32b81fa03e0fd55b7d8641ac7d348eb2694f
John Wilger <johnwilger@gmail.com>
2012-03-10 12:30:27 -0800

    GivenDriver documentation

--- lib/kookaburra/given_driver.rb
+++ lib/kookaburra/given_driver.rb
@@ -6,11 +6,33 @@ class Kookaburra
   # simple mapping to your application's API, a method in the GivenDriver may be
   # comprised of several distinct API calls as well as access to Kookaburra's
   # test data store.
+  #
+  # @abstract Subclass and implement your Given DSL
+  #
+  # @example GivenDriver subclass
+  #   module MyApp
+  #     module Kookaburra
+  #       class GivenDriver < ::Kookaburra::GivenDriver
+  #         def a_widget(name, attributes = {})
+  #           # Set up the data that will be passed to the API by merging any
+  #           # passed attributes into the default data.
+  #           data = {:name => 'Foo', :description => 'Bar baz'}.merge(attributes)
+  #
+  #           # Call the API method and get the resulting response as Ruby data.
+  #           result = api.create_widget(data)
+  #
+  #           # Store the resulting widget data in the TestData object, so that
+  #           # it can be referenced in other operations.
+  #           test_data.widgets[name] = result
+  #         end
+  #       end
+  #     end
+  #   end
   class GivenDriver
     extend DependencyAccessor
 
     # It is unlikely that you would call #initialize yourself; your GivenDriver
-    # object is instantiated for you by your {Kookaburra} object.
+    # object is instantiated for you by {Kookaburra#given}.
     #
     # @option options [Kookaburra::TestData] the test data store
     # @option options [Kookaburra::APIDriver] the APIDriver subclass to be used
@@ -21,10 +43,18 @@ class Kookaburra
 
     protected
 
+    # A reference to the {Kookaburra::TestData} object that this GivenDriver
+    # instance was created with.
+    #
     # @attribute [r]
+    # @return [Kookaburra::TestData]
     dependency_accessor :test_data
 
+    # A reference to the {Kookaburra::APIDriver} that this GivenDriver instance
+    # was created with.
+    #
     # @attribute [r]
+    # @return [Kookaburra::APIDriver]
     dependency_accessor :api
   end
 end
