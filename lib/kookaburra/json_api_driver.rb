134e6ac7050dd4b4efd37ca70b757174e5541463
John Wilger <johnwilger@gmail.com>
2012-03-21 21:55:02 -0700

    Updated API docs for JsonApiDriver

--- lib/kookaburra/json_api_driver.rb
+++ lib/kookaburra/json_api_driver.rb
@@ -3,7 +3,20 @@ require 'delegate'
 require 'active_support/json'
 
 class Kookaburra
+  # Delegates all methods (by default) to and instance of
+  # {Kookaburra::APIDriver}.
+  #
+  # Expects the application's API to accept and respond with JSON formatted
+  # data. All methods will decode the response body using
+  # `ActiveSupport::JSON.decode`. Methods that take input data ({#post} and
+  # {#put}) will encode the post data using `ActiveSupport::JSON.encode`.
   class JsonApiDriver < SimpleDelegator
+    #
+    # Sets both the "Content-Type" and "Accept" headers to "application/json".
+    #
+    # @option options [Kookaburra::APIDriver] :api_driver (Kookaburra::APIDriver.new)
+    #   The APIDriver instance to be delegated to. Changing this is probably
+    #   only useful for testing.
     def initialize(options = {})
       api_driver = options[:api_driver] || APIDriver.new(:app_host => options[:app_host])
       api_driver.headers.merge!(
