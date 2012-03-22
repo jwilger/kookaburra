1cee2a6a8ba32c27b383b931262fe32b9e9aa374
John Wilger <johnwilger@gmail.com>
2012-03-21 18:19:49 -0700

    comment

--- lib/kookaburra/json_api_driver.rb
+++ lib/kookaburra/json_api_driver.rb
@@ -34,8 +34,10 @@ class Kookaburra
     private
 
     def request(type, path, data = nil, *args)
+      # don't want to send data to methods that don't accept it
       args = [path, encode(data), args].flatten.compact
       output = __getobj__.send(type, *args)
+
       decode(output)
     end
 
