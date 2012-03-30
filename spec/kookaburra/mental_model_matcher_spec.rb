0c9c31b7d884ef7df1fcc8de3b8b2e918b9ad250
Sam Livingston-Gray <geeksam@gmail.com>
2012-03-30 11:56:41 -0700

    Refactor specs

--- spec/kookaburra/mental_model_matcher_spec.rb
+++ spec/kookaburra/mental_model_matcher_spec.rb
@@ -4,119 +4,208 @@ describe Kookaburra::MentalModel::Matcher do
   let(:mm) { Kookaburra::MentalModel.new }
 
   def matcher_for(collection_key)
-    Kookaburra::MentalModel::Matcher.new(mm, collection_key)
+    Kookaburra::MentalModel::Matcher.new(mm, collection_key).tap do |m|
+      m.matches?(target)
+    end
   end
-  
-  let(:matcher) { matcher_for(:widgets).tap { |m| m.matches?(target) } }
+
+  def pp_array(array)
+    array = array.sort if array.all? { |e| e.respond_to?(:<=>) }
+    array.inspect
+  end
+
+  let(:matcher) { matcher_for(:widgets) }
 
   let(:foo) { { :name => 'Foo', :type => 'widget' } }
   let(:bar) { { :name => 'Bar', :type => 'widget' } }
   let(:yak) { { :name => 'Yak', :type => 'widget' } }
 
-  context "when the named collection has nothing in it" do
-    context "and the actual list is an empty array" do
+  def self.it_matches
+    it "matches (short form)" do
+      matcher.matches?(target).should be_true
+    end
+  end
+
+  context "expecting [];" do
+    context "for []" do
       let(:target) { [] }
 
-      it "#matches? returns true when given an empty array" do
+      it "matches when given an empty array" do
         matcher.matches?(target).should be_true
       end
     end
 
-    context "and the actual list is [foo] (foo not in mental model)" do
+    context "for [foo] (foo not in mental model)" do
       let(:target) { [foo] }
-      it "#matches? returns true" do
+
+      it "matches" do
         matcher.matches?(target).should be_true
       end
     end
   end
 
-  context "when the named collection has foo in it" do
+  context "expecting [foo];" do
     before(:each) do
       mm.widgets[:foo] = foo
     end
 
-    context "and the actual list is an empty array (foo missing)" do
+    context "for [] (foo missing)" do
       let(:target) { [] }
 
-      it '#matches? returns false' do
+      it 'does not match' do
         matcher.matches?(target).should be_false
       end
 
-      it "#failure_message_for_should complains about missing element" do
+      it "complains about missing foo" do
         matcher.failure_message_for_should.should == <<-EOF
 expected widgets to match the user's mental model, but:
-expected to be present:         #{[foo].inspect}
-the missing elements were:      #{[foo].inspect}
+expected to be present:         #{pp_array([foo])}
+the missing elements were:      #{pp_array([foo])}
 EOF
       end
     end
 
-    context "and the actual list is [foo] (OK)" do
+    context "for [foo] (OK)" do
       let(:target) { [foo] }
 
-      it "#matches? returns true" do
+      it "matches" do
         matcher.matches?(target).should be_true
       end
     end
 
-    context "and the actual list is [foo, bar] (bar not in mental model)" do
+    context "for [foo, bar] (bar not in mental model)" do
       let(:target) { [foo, bar] }
 
-      it "#matches? returns true" do
+      it "matches" do
         matcher.matches?(target).should be_true
       end
     end
   end
 
-  context "when the named collection has foo in it, and deleted bar" do
+  context "expecting [foo, bar];" do
+    before(:each) do
+      mm.widgets[:foo] = foo
+      mm.widgets[:bar] = bar
+    end
+
+    context "for []" do
+      let(:target) { [] }
+      
+      it "does not match" do
+        matcher.matches?(target).should be_false
+      end
+
+      it 'complains about missing foo, bar' do
+        matcher.failure_message_for_should.should == <<-EOF
+expected widgets to match the user's mental model, but:
+expected to be present:         #{pp_array([foo, bar])}
+the missing elements were:      #{pp_array([foo, bar])}
+EOF
+      end
+    end
+
+    context "for [bar] (foo missing)" do
+      let(:target) { [bar] }
+      
+      it "does not match" do
+        matcher.matches?(target).should be_false
+      end
+
+      it 'complains about missing foo' do
+        matcher.failure_message_for_should.should == <<-EOF
+expected widgets to match the user's mental model, but:
+expected to be present:         #{pp_array([foo, bar])}
+the missing elements were:      #{pp_array([foo])}
+EOF
+      end
+    end
+
+    context "for [foo, bar] (OK)" do
+      let(:target) { [foo, bar] }
+      
+      it "does not match" do
+        matcher.matches?(target).should be_true
+      end
+    end
+
+    context "for [foo, bar, yak] (foo, bar expected; yak not in mental model)" do
+      let(:target) { [foo, bar, yak] }
+      
+      it "matches" do
+        matcher.matches?(target).should be_true
+      end
+    end
+  end
+
+  context "expecting [foo], not expecting [bar];" do
     before(:each) do
       mm.widgets[:foo] = foo
       mm.widgets[:bar] = bar
       mm.widgets.delete(:bar)
     end
 
-    context "when the actual list is an empty array (foo missing)" do
+    context "for [] (foo missing)" do
       let(:target) { [] }
       
-      it "#matches? returns false" do
+      it "does not match" do
         matcher.matches?(target).should be_false
       end
 
-      it '#failure_message_for_should'
+      it 'complains about missing foo' do
+        matcher.failure_message_for_should.should == <<-EOF
+expected widgets to match the user's mental model, but:
+expected to be present:         #{pp_array([foo])}
+the missing elements were:      #{pp_array([foo])}
+EOF
+      end
     end
 
-    context "when the actual list is [bar] (foo missing, bar deleted)" do
+    context "for [bar] (foo missing, bar deleted)" do
       let(:target) { [bar] }
       
-      it "#matches? returns false" do
+      it "does not match" do
         matcher.matches?(target).should be_false
       end
 
-      it '#failure_message_for_should'
+      it 'complains about missing foo and extra bar' do
+        matcher.failure_message_for_should.should == <<-EOF
+expected widgets to match the user's mental model, but:
+expected to be present:         #{pp_array([foo])}
+the missing elements were:      #{pp_array([foo])}
+expected to not be present:     #{pp_array([bar])}
+the unexpected extra elements:  #{pp_array([bar])}
+EOF
+      end
     end
 
-    context "when the actual list is [foo, bar] (bar deleted)" do
+    context "for [foo, bar] (bar deleted)" do
       let(:target) { [foo, bar] }
       
-      it "#matches? returns false" do
+      it "does not match" do
         matcher.matches?(target).should be_false
       end
 
-      it '#failure_message_for_should'
+      it 'complains about extra bar' do
+        matcher.failure_message_for_should.should == <<-EOF
+expected widgets to match the user's mental model, but:
+expected to not be present:     #{pp_array([bar])}
+the unexpected extra elements:  #{pp_array([bar])}
+EOF
+      end
     end
 
-    context "when the actual list is [foo] (foo expected)" do
+    context "for [foo] (foo expected)" do
       let(:target) { [foo] }
       
-      it "#matches? returns true" do
+      it "matches" do
         matcher.matches?(target).should be_true
       end
     end
 
-    context "when the actual list is [foo, yak] (foo expected; yak not in mental model)" do
+    context "for [foo, yak] (foo expected; yak not in mental model)" do
       let(:target) { [foo, yak] }
       
-      it "#matches? returns true" do
+      it "matches" do
         matcher.matches?(target).should be_true
       end
     end
