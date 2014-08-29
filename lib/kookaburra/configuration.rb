06180e9bafec7c3c2a2db209f13f9cdbe3ccff7e
John Wilger <johnwilger@gmail.com>
2014-08-29 11:24:14 -0700

    Adds api_client_driver_class attr to the configuration

--- lib/kookaburra/configuration.rb
+++ lib/kookaburra/configuration.rb
@@ -7,6 +7,13 @@ class Kookaburra
   class Configuration
     extend DependencyAccessor
 
+    # The class to use as your APIClientDriver
+    #
+    # @attribute [rw] api_client_driver_class
+    # @raise [Kookaburra::ConfigurationError] if you try to read this attribute
+    #   without it having been set
+    dependency_accessor :api_client_driver_class
+
     # The class to use as your GivenDriver
     #
     # @attribute [rw] given_driver_class
