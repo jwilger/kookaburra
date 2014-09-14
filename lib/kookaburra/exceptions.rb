bf2e4d61872b7da892170c48e815021b735182c2
John Wilger <johnwilger@gmail.com>
2014-09-13 18:33:34 -0700

    Removes unused exceptions and adds a bit of documentation

--- lib/kookaburra/exceptions.rb
+++ lib/kookaburra/exceptions.rb
@@ -1,16 +1,22 @@
 class Kookaburra
+  # Raised when accessing unknown mental model data
   # @private
   class UnknownKeyError < ArgumentError; end
+
+  # Raised when there is a problem with the Kookaburra runtime configuration
   # @private
   class ConfigurationError < StandardError; end
+
+  # Raised when an API or UI request results in an enexpected HTTP response
   # @private
   class UnexpectedResponse < RuntimeError; end
+
+  # Raised when an assertion fails inside the drivers.
   # @private
   class AssertionFailed < RuntimeError; end
-  # @private
-  class ComponentNotFound < RuntimeError; end
-  # @private
-  class NullBrowserError < ConfigurationError; end
+
+  # Raised when calling `api` or `ui` test helpers and named applications are
+  # configured
   # @private
   class AmbiguousDriverError < StandardError; end
 end
